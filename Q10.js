5a1224e3c4c94df29c09ecb7888a11ac92766644
Paras Arya <parasarya289@gmail.com>
2023-02-16 18:42:22 +0530

    multiplyByTen

---
+++ Q10.js
@@ -0,0 +1,6 @@
+// Write a function which takes in a number as input and returns it after multiplying by 10.
+
+const multiplyByTen = (num) => num*10 
+
+console.log(multiplyByTen(20)) // 200
+console.log(multiplyByTen(40)) // 400
