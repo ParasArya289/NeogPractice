17131aa94ad93eecce6890a6327a61d6acb5ade2
Paras Arya <parasarya289@gmail.com>
2023-02-16 18:40:03 +0530

    isMultipleOfThree

---
+++ Q9.js
@@ -0,0 +1,6 @@
+// Write a function that takes a number as input and determines if it is a multiple of 3.
+
+const isMultipleOfThree = (num) => !(num%3);
+
+console.log(isMultipleOfThree(5)); // false
+console.log(isMultipleOfThree(9)); // true
