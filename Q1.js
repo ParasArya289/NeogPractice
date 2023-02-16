8d21ae2e80423e717dadd7cebe47664704eb4247
Paras Arya <parasarya289@gmail.com>
2023-02-16 17:59:34 +0530

    drivingAge

---
+++ Q1.js
@@ -0,0 +1,3 @@
+// Write a function that takes a user's age and determines if they are old enough to vote (age 18 or older).
+
+const drivingAge = (age) => age>=18? 'Eligilbe to vote' : 'Not eligible to vote';
