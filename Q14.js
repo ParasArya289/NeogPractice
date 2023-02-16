695d95f2f2ea3b94040c8fa88688ae5d8d979098
Paras Arya <parasarya289@gmail.com>
2023-02-16 19:00:52 +0530

    array destructuring

---
+++ Q14.js
@@ -0,0 +1,9 @@
+// Given an array numbers containing the numbers 1, 2, and 3. Use array destructuring to log each number to the console.
+
+const numbers = [1, 2, 3];
+
+const [a,b,c] = numbers
+
+console.log(a); // logs 1 to the console
+console.log(b); // logs 2 to the console
+console.log(c); // logs 3 to the console
