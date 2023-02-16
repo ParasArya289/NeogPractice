eea04785d61f9ce9dbd1637465ee834f715b3220
Paras Arya <parasarya289@gmail.com>
2023-02-16 18:32:47 +0530

    isBetweenOneAndTen

---
+++ Q7.js
@@ -0,0 +1,5 @@
+// Write a function that takes a number as input and determines if it is between 1 and 10.
+
+const isBetweenOneAndTen = (num) => num > 1 && num < 10  
+
+console.log(isBetweenOneAndTen(2))
