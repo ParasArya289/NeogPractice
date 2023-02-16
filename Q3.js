61b547d2b5d92ec96dd704f6e122e482b764034a
Paras Arya <parasarya289@gmail.com>
2023-02-16 18:09:21 +0530

    posNeg

---
+++ Q3.js
@@ -0,0 +1,5 @@
+// Write a function that takes a number as input and determines if it is positive or negative.
+
+const posNeg = (num) => num>=0? 'Positive Number' : 'Negative Number';
+
+console.log(posNeg(0))
