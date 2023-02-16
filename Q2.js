16fda13bc0af46e9822fd2993d9e29b6731b37a6
Paras Arya <parasarya289@gmail.com>
2023-02-16 18:06:30 +0530

    greaterNum

---
+++ Q2.js
@@ -0,0 +1,5 @@
+// Write a function that takes two numbers as input and determines which one is greater.
+
+const greaterNum = (a,b) => a>b?`${a} is greater then ${b}`: `${b} is greater then ${a}`
+
+console.log(greaterNum(5,2))
