2cd2f7089ab9fb58d289d3323d4664912b90c100
Paras Arya <parasarya289@gmail.com>
2023-02-17 23:07:59 +0530

    concatenateStrings

---
+++ ES6 Practice set 2/Q10.js
@@ -0,0 +1,5 @@
+// Write a function that takes two strings as input and concatenates them together.
+
+const concatenateStrings = (s1,s2) => s1+s2; 
+
+console.log(concatenateStrings("hello", "world")); // "helloworld"
