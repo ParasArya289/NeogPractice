2263448be991240c7435162dcd7e7a893382f263
Paras Arya <parasarya289@gmail.com>
2023-02-17 23:15:09 +0530

    sumFirstElement

---
+++ ES6 Practice set 2/Q13.js
@@ -0,0 +1,5 @@
+// Write a function that takes an array and a number and returns the sum of first element and the number.
+
+const sumFirstElement = ([a],num) => a+num;
+
+console.log(sumFirstElement([1, 2, 3], 5)); // 6
