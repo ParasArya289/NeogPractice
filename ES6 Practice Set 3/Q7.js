948fda75941a481788eb7a7b12bcebe284889821
Paras Arya <parasarya289@gmail.com>
2023-02-20 23:12:45 +0530

    printLastFive

---
+++ ES6 Practice Set 3/Q7.js
@@ -0,0 +1,6 @@
+// Write a function to return the last 5 characters as an array from the given array.
+
+const printLastFive = (arr) => arr.slice(-5) 
+
+console.log(printLastFive([0, 1, 1, 2, 3, 5, 8]))
+// [1, 2, 3, 5, 8]
