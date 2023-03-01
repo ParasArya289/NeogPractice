10eaf1c683773095d54489e1b4e9dcbebe699ca1
Paras Arya <parasarya289@gmail.com>
2023-03-01 22:55:06 +0530

    filterBySalary

---
+++ FP Practice Set 1/Q12.js
@@ -0,0 +1,14 @@
+// 7. Write an ES6 function that takes an array of objects containing employee information (name, age, salary) and returns an array with only the employees who earn a salary above 22000 and age is above 25.
+
+const filterBySalary = (arr,a,b) => arr.filter(({salary,age})=>salary>a && age>b)
+
+const employees = [
+  {name: "Abhay", age: 25, salary: 20000},
+  {name: "Joice", age: 30, salary: 35000},
+  {name: "Reena", age: 35, salary: 15000},
+  {name: "Jeena", age: 40, salary: 50000}
+];
+
+const filteredEmployees = filterBySalary(employees, 22000, 25);
+console.log(filteredEmployees);
+// Output: [{name: "Joice", age: 30, salary: 35000}, {name: "Jeena", age: 40, salary: 50000}]
