6b4b3bd8593e5d0018039baa1f06720a8ab9b69a
Paras Arya <parasarya289@gmail.com>
2023-03-09 23:05:49 +0530

    getAllElectronicProducts

---
+++ FP Practice Set 3/Q1/Q4.js
@@ -0,0 +1,15 @@
+const products = [
+  { name: "Toothbrush", price: 29, category: "health" },
+  { name: "Coffee Maker", price: 99, category: "home" },
+  { name: "iPad", price: 799, category: "electronics" },
+  { name: "Smartwatch", price: 199, category: "electronics" },
+];
+
+// Your code here
+
+const getAllElectronicProducts = (arr) =>
+  arr.filter(({ category }) => category === "electronics");
+
+const electronicProducts = getAllElectronicProducts(products);
+console.log(electronicProducts);
+// Output: [{ name: "iPad", price: 799, category: "electronics" }, { name: "Smartwatch", price: 199, category: "electronics" }]
