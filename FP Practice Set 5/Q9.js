e6adae1e23d939c6b8447ef35693e0dd078d8416
Paras Arya <parasarya289@gmail.com>
2023-03-13 23:52:50 +0530

    Products Inventory

---
+++ FP Practice Set 5/Q9.js
@@ -0,0 +1,47 @@
+// Create an array of objects representing products in an online store. Each object has the following properties: id, name, price, and category. Write a React component that takes the array of products as input and generates an ordered list of products, where each list item displays the product's name, price, and category.
+
+const products = [
+  {
+    id: 1,
+    name: "Black T-Shirt",
+    price: 540,
+    category: "Clothing",
+  },
+  {
+    id: 2,
+    name: "Quad Copter",
+    price: 2000,
+    category: "Toys",
+  },
+  {
+    id: 3,
+    name: "5AM Club",
+    price: 220,
+    category: "Books",
+  },
+  {
+    id: 4,
+    name: "Areca Palm",
+    price: 540,
+    category: "Plants",
+  },
+];
+
+const Q9 = () => {
+  return (
+    <div>
+      <h1>Products Inventory</h1>
+      <ol>
+        {products.map((el) => (
+          <li>
+            <h4>{el.name}</h4>
+            <span>
+              Rs.{el.price},{el.category}
+            </span>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+export default Q9;
