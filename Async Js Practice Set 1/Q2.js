93756c122c5a9cad91d300afabafeb191cd27862
Paras Arya <parasarya289@gmail.com>
2023-03-18 23:27:13 +0530

    delayedAddition

---
+++ Async Js Practice Set 1/Q2.js
@@ -0,0 +1,6 @@
+// Write a function ‘delayedAddition’ that takes in two numbers and consoles their sum after a delay of 4 seconds using setTimeout. You can practice this question in any JS editor or your browser console.
+
+const delayedAddition = (a, b) => setTimeout(() => console.log(a + b), 1000);
+
+const res = delayedAddition(2, 3);
+//5
