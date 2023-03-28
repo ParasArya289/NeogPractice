bf428e934367d59dfa9673407c08820f0fe907ca
Paras Arya <parasarya289@gmail.com>
2023-03-28 21:39:40 +0530

    utils

---
+++ React Practice Set 1/utils/Q3utils.js
@@ -0,0 +1,7 @@
+export const header = "Stationary Items";
+export const items = ["pen", "pencil", "ruler", "eraser"];
+export const listStyle = {
+  border: "1px solid black",
+  color: "white",
+  borderRadius: "20px"
+};
---
+++ React Practice Set 1/utils/Q4utils.js
@@ -0,0 +1,11 @@
+export const imageLink =
+  "https://cdn.pixabay.com/photo/2023/03/18/10/43/plum-blossoms-7860381_1280.jpg";
+export const caption = "Spring Flowers";
+export const container = {
+  margin: "auto",
+  width: "300px",
+  borderRadius: "20px",
+  padding: "20px",
+  backgroundColor: "#fee2e2",
+  color: "#450a0a"
+};
