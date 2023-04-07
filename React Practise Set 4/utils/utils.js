63a72da1cd4deae6f3bf1888d0e6124074981685
Paras Arya <parasarya289@gmail.com>
2023-04-07 23:53:13 +0530

    utils

--- React Practise Set 4/utils/utils.js
+++ React Practise Set 4/utils/utils.js
@@ -55,4 +55,11 @@ export const characters = {
     { id: 3, name: "Banana", category: "Fruit" },
     { id: 4, name: "Broccoli", category: "Vegetable" }
   ];
+
+  export const todoItems = [
+    {id: 1, task: "Writing 1-page poem", isDone: true},
+    {id: 2, task: "Gym", isDone: false},
+    {id: 3, task: "Shopping", isDone: false},
+    {id: 4, task: "Standup call", isDone: true},
+  ]
   
