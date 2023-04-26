c3eeb5a13ed975cd8f34d1ad1975d857a71a4726
Paras Arya <parasarya289@gmail.com>
2023-04-26 23:41:59 +0530

    Q3

---
+++ CSS Practice Set 1/Jsx/Q3.js
@@ -0,0 +1,15 @@
+import "../styles/q3.css";
+
+export const Q3 = () => {
+  return (
+    <>
+      <ul className="list">
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+      </ul>
+    </>
+  );
+};
