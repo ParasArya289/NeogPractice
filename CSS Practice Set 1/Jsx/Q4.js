f6de7da9605d4201e7e687d553b1a9a3bc146d0d
Paras Arya <parasarya289@gmail.com>
2023-04-28 00:00:23 +0530

    Q4

---
+++ CSS Practice Set 1/Jsx/Q4.js
@@ -0,0 +1,9 @@
+import "../styles/q4.css";
+export const Q4 = () => {
+  return (
+    <>
+      <input className="search-bar" placeholder="Search" />
+      <button className="btn">Search</button>
+    </>
+  );
+};
