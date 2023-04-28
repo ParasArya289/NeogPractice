1231708d5de43f550248f40ac9641f79797fc813
Paras Arya <parasarya289@gmail.com>
2023-04-28 23:48:33 +0530

    Q5

---
+++ CSS Practice Set 1/Jsx/Q5.js
@@ -0,0 +1,17 @@
+import "../styles/q5.css";
+export const Q5 = () => {
+  return (
+    <>
+      <div className="card-container">
+        <img
+          className="card-image"
+          src="https://via.placeholder.com/350x150"
+          alt="img"
+        />
+        <h3 className="card-heading">This is Card</h3>
+        <p className="card-description">Example description</p>
+        <button className="card-btn">Click me</button>
+      </div>
+    </>
+  );
+};
